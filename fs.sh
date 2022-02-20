bef13d94a6bc1a8ca06b79a7f15db25b0edcbcb0
Damian Ciepiela <dciepiela@yahoo.com>
2022-02-20 22:55:50 +0100

    feat: script for regex file search in directory

---
+++ fs.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+if (( $# < 1 ))
+then
+	echo "Usage: fs.sh <name> [<directory>]"
+fi
+
+FILE=$1
+DIR=""
+if [[ -d $2 ]]
+then
+	DIR=$2
+else
+	DIR=$(pwd)
+fi
+
+look_for_file() {
+	local FILE_NAME=$1
+	local ENTRY=$2
+	
+	if [ -z "$(ls -A $ENTRY)" ]
+	then
+		return 0
+	fi
+
+	if [[ -d $ENTRY ]]
+	then
+		for INNER_ENTRY in $ENTRY/*
+			do
+				look_for_file $FILE_NAME $INNER_ENTRY
+			done
+	else
+		local ENTRY_NAME=$(basename $ENTRY)
+		if [[ "$ENTRY_NAME" == *"$FILE_NAME"* ]]
+		then
+			echo $ENTRY
+		fi	
+	fi
+}
+
+look_for_file $FILE $DIR
+
